d0b9ed8861c68f03c26cec44b6cadb91b27852ae
Meena Erian <meena5.erian@gmail.com>
2021-01-24 14:01:23 +0400

    Show databases in admin panel

--- reflector/admin.py
+++ reflector/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Database
 
 # Register your models here.
+
+class DatabaseAdmin(admin.ModelAdmin):
+    None
+
+admin.site.register(Database, DatabaseAdmin)
