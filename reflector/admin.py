174c124c1db747aea70e84d0dca766fef97ed464
Meena Erian <meena5.erian@gmail.com>
2021-01-24 14:21:41 +0400

    use ace editor

--- reflector/admin.py
+++ reflector/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
+from django import forms
 from .models import Database
+from django_ace import AceWidget
 
 # Register your models here.
 
+class DatabaseForm(forms.ModelForm):
+    class Meta:
+        model = Database
+        widgets = {
+            'config': AceWidget(mode='json')
+        }
+        fields = ['handle', 'description', 'config']
+
 class DatabaseAdmin(admin.ModelAdmin):
+    form = DatabaseForm
     def get_form(self, request, obj=None, **kwargs):
         if not obj:
             kwargs['exclude'] = ['connection_verified']
