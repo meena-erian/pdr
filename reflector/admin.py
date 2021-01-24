20488308f7985578516e6207446dae78bc62b2d1
Meena Erian <meena5.erian@gmail.com>
2021-01-24 14:12:23 +0400

    lock some fields

--- reflector/admin.py
+++ reflector/admin.py
@@ -4,6 +4,14 @@ from .models import Database
 # Register your models here.
 
 class DatabaseAdmin(admin.ModelAdmin):
-    None
+    def get_form(self, request, obj=None, **kwargs):
+        if not obj:
+            kwargs['exclude'] = ['connection_verified']
+        return super(DatabaseAdmin, self).get_form(request, obj, **kwargs)
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['source']
+        else:
+            return []
 
 admin.site.register(Database, DatabaseAdmin)
