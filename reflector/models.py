da9c7190d1fb6f202993d254f5a4f8051f2c2912
Meena Erian <meena5.erian@gmail.com>
2021-01-26 18:43:22 +0400

    broadcasting table data model

--- reflector/models.py
+++ reflector/models.py
@@ -77,3 +77,19 @@ class Database(models.Model):
             self.mount().connect()
         except Exception as e:
             raise ValidationError('Failed to connect to database: {0}'.format(e))
+
+class BroadcastingTable(models.Model):
+    source_database = models.ForeignKey(Database, on_delete=models.CASCADE)
+    source_table = models.CharField(max_length=200)
+    description = models.CharField(max_length=500, blank=True)
+    fk_name = models.CharField(max_length=500, blank=True, null=True)
+    def __str__(self):
+        return self.source_database.handle + '.' + self.source_table
+    def clean(self):
+        if not hasattr(self, 'source_database'):
+            raise ValidationError('Please select source database')
+        ### Check if selected table exists in selected database. if not raise ValidationError
+        ### Install event listeners to the table
+    def delete(self):
+        ### try to remove event listeners from the databases table. If failed, raise ValidationError
+        super(BroadcastingTable, self).delete()
