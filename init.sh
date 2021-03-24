50928b72f3765c8163bf7ef5171f942f0b4e89fb
Meena Erian <meena5.erian@gmail.com>
2021-03-24 15:03:43 +0400

    add initial installation scripts

---
+++ init.sh
@@ -0,0 +1,10 @@
+pip install django
+pip install django-ace
+pip install psycopg2
+pip install pytz
+pip install SQLAlchemy
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic
+python manage.py createsuperuser
+python manage.py runserver
